e07bcc06641e1aba2da6ad904d050e53725732d4
Victoria045 <victoriaberyl45@gmail.com>
2022-07-26 20:54:46 +0300

    configure dependncies

--- users/settings.py
+++ users/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lktpo!s%=03bo(gr3h#)s9^w#bt^ee=+6i4kzz74ppd0&@^j%e'
+# SECRET_KEY = 'django-insecure-lktpo!s%=03bo(gr3h#)s9^w#bt^ee=+6i4kzz74ppd0&@^j%e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
